5f8ee33f2da7d6b5ab26c89567c21390aec9a4aa
nathan-andrei <nathan_andrei_asnan@dlsu.edu.ph>
2023-10-26 21:41:02 +0800

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -9,13 +9,17 @@ async function initializeUserStatus(){
         }
     })
 
+    //TO THE FRONT END WHO DID THIS:
+    //I AM SORRY
+    //I HAD TO DO HANDLEBARS FOR TESTING MY LOG OUT, AND IT CONFLICTS
+    //WITH THIS APPROACH. I AM SO SORRY
     if(response.status == 200){
-        regLog.style.visibility = "hidden";
-        myAccount.style.visibility = "visible";      
+        //regLog.style.visibility = "hidden";
+        //myAccount.style.visibility = "visible";      
     }else if(response.status == 400){
         console.log("failed to get current user")
-        regLog.style.visibility = "visible";
-        myAccount.style.visibility = "hidden";
+        //regLog.style.visibility = "visible";
+        //myAccount.style.visibility = "hidden";
     }
 }
 
